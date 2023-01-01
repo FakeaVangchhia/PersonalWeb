078f391e6a82d2484201f36e573fc2c07748a8a9
Fakea Vangchhia <fakafka44@gmail.com>
2023-01-01 23:36:30 +0530

    new year

--- myWeb/settings.py
+++ myWeb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.1/howto/deplxoyment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h#!(-9cx#^5jnk0n9um*i-xrix9k7v!%4yxst$bsq2yqs$our)'
